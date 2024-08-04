a4cb7260177f869770251fa817eb30b8c74d6716
lucasmachados <lucasmachadosilva93@gmail.com>
2024-08-04 06:53:36 -0300

    02

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = scanner.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = scanner.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = scanner.nextInt();
 
-        try{
-            contar(parametroUm, parametroDois);
+            try{
+                contar(parametroUm, parametroDois);
 
-        }catch (ParametrosInvalidosException e){
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+            }catch (ParametrosInvalidosException e){
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+            }
+            scanner.close();
         }
     }
 
